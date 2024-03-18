0c3ac9b15621687d61ee04196ec78321af1020aa
Sachintha Gunaratne <sachinthasl99@gmail.com>
2024-03-18 11:29:44 +0530

    fix: avoids unwanted call to verifyRefreshToken

--- src/components/PersistLogin.js
+++ src/components/PersistLogin.js
@@ -1,45 +1,40 @@
 import { Outlet } from "react-router-dom";
-import { useState, useEffect } from "react";
-import useRefreshToken from '../hooks/useRefreshToken';
-import useAuth from '../hooks/useAuth';
+import { useState, useEffect, useRef } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
 
 const PersistLogin = () => {
-    const [isLoading, setIsLoading] = useState(true);
-    const refresh = useRefreshToken();
-    const { auth, persist } = useAuth();
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth, persist } = useAuth();
+  const isMounted = useRef(true);
+  const isAlreadyCalled = useRef(false);
 
-    useEffect(() => {
-        let isMounted = true;
+  useEffect(() => {
+    const verifyRefreshToken = async () => {
+      if (isAlreadyCalled.current) return;
+      isAlreadyCalled.current = true;
+      try {
+        console.log("verifying refresh token");
+        await refresh();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        isMounted.current && setIsLoading(false);
+        isAlreadyCalled.current = false;
+      }
+    };
 
-        const verifyRefreshToken = async () => {
-            try {
-                await refresh();
-            }
-            catch (err) {
-                console.error(err);
-            }
-            finally {
-                isMounted && setIsLoading(false);
-            }
-        }
+    !auth?.accessToken && persist ? verifyRefreshToken() : setIsLoading(false);
 
-        // persist added here AFTER tutorial video
-        // Avoids unwanted call to verifyRefreshToken
-        !auth?.accessToken && persist ? verifyRefreshToken() : setIsLoading(false);
+    return () => {
+      isMounted.current = false;
+    };
+  }, [auth, persist, refresh]);
 
-        return () => isMounted = false;
-    }, [])
+  return (
+    <>{!persist ? <Outlet /> : isLoading ? <p>Loading...</p> : <Outlet />}</>
+  );
+};
 
-    return (
-        <>
-            {!persist
-                ? <Outlet />
-                : isLoading
-                    ? <p>Loading...</p>
-                    : <Outlet />
-            }
-        </>
-    )
-}
-
-export default PersistLogin
+export default PersistLogin;
